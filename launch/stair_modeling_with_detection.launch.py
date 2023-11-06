8622a6ed741495d7d63b890fe33d7048ef22f9d8
nimiCurtis <nimicu21@gmail.com>
2023-11-06 21:12:37 +0200

    update

--- launch/stair_modeling_with_detection.launch.py
+++ launch/stair_modeling_with_detection.launch.py
@@ -1,11 +1,7 @@
-
-import os
-from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
