66480fc3f2800f6d5caa91d90271d33fd891ad20
nimiCurtis <nimicu21@gmail.com>
2023-12-14 09:53:24 +0200

    update

--- src/stair_modeling.cpp
+++ src/stair_modeling.cpp
@@ -43,7 +43,7 @@ int main(int argc, char * argv[])
   // Set the desired number of threads for the executor
   rclcpp::ExecutorOptions exec_options;
   
-  const size_t num_threads = 8; // Change this number to your desired value
+  const size_t num_threads = 4; // Change this number to your desired value
   auto executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>(exec_options,num_threads);
   executor->add_node(stair_modeling);
   executor->spin();
